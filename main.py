d2a01c219dd8cd9787b896ea6e10b4ae5479c21f
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-06-25 16:12:27 +0300

    написан основной код программы\\the main program code has been written

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 from utils import reg_check, get_reg_data
+
+users_list = []
+check_phones_list = []
+check_emails_list = []
+check_unique_list = [None, None, check_phones_list, check_emails_list]
+data_types = ["имя", "фамилия", "телефон", "мейл"]
+
+reg_data_list = get_reg_data()
+# print(reg_data_list)
+while len(users_list) < 3:
+    user_data_list = []
+    data_counter = 0
+
+    while data_counter < 4:
+        user_data = input(f"ВВедите ваши данные: {data_types[data_counter]}\n")
+        user_data_ok = reg_check(user_data, reg_data_list[data_counter], users_list, check_unique_list[data_counter])
+        if user_data_ok:
+            data_counter += 1
+            user_data_list.append(user_data_ok)
+            pass
+        else:
+            continue
+    users_list.append(user_data_list)
+    check_phones_list.append(user_data_list[2])
+    check_emails_list.append(user_data_list[3])
+    print()
+
+[print(user) for user in users_list]
