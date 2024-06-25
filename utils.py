67169ccdfad05d9e30cc0a8b05ee1ebab05c7091
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-06-25 14:26:45 +0300

    сделано форматирование\\formatting has been done

--- utils.py
+++ utils.py
@@ -19,7 +19,8 @@ def reg_check(user_data, reg_pattern, users_list, data_to_check=None):
     while True:
         if reg_pattern.match(user_data):  # получаем булевое значение|we get a Boolean value
             if len(users_list) > 0 and data_to_check:
-                if not check_unique_data(user_data, data_to_check):  # принимает на основании аргументов булевое значение по ретерну
+                if not check_unique_data(user_data,
+                                         data_to_check):  # принимает на основании аргументов булевое значение по ретерну
                     user_data = input()
                     continue
         else:
