6efbddd6462b4a90ef6a31872a2161c2ff726d56
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-06-25 14:23:45 +0300

    создана функция для проверки уникальности введенных данных\\a function has been created to verify the uniqueness of the entered data

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def reg_check(user_data, reg_pattern, users_list, data_to_check=None):
     while True:
         if reg_pattern.match(user_data):  # получаем булевое значение|we get a Boolean value
             if len(users_list) > 0 and data_to_check:
-                if not check_unique_data(user_data, data_to_check):
+                if not check_unique_data(user_data, data_to_check):  # принимает на основании аргументов булевое значение по ретерну
                     user_data = input()
                     continue
         else:
@@ -29,3 +29,13 @@ def reg_check(user_data, reg_pattern, users_list, data_to_check=None):
         print(f"{user_data} - данные приняты")
 
 
+''' проверяет уникальность введенных данных|checks the uniqueness of the entered data'''
+
+
+def check_unique_data(user_data, data_to_check):
+    if user_data in data_to_check:
+        print("Такие данные уже есть")
+        print("введите уникальные данные!")
+        return False
+    else:
+        return True
