b36159fb36ebc3d3f37da90a29ffe8e96b943ea0
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-06-25 14:12:29 +0300

    создана функция для проверки корректности ввода данных пользователем\\A function has been created to check the correctness of user input

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import re
 
-'''returns regular expression patterns to validate the entered data\
-\возвращает паттерны регулярных выражений для проверки введенных данных'''
+'''returns regular expression patterns to validate the entered data|возвращает паттерны регулярных выражений для проверки введенных данных'''
 
 
 def get_reg_data():
@@ -12,5 +11,21 @@ def get_reg_data():
     reg_patterns_list = [reg_name, reg_surname, reg_phone, reg_email]
     return reg_patterns_list
 
+
+'''проверяет данные при вводе пользователем|checks the data when the user enters it'''
+
+
+def reg_check(user_data, reg_pattern, users_list, data_to_check=None):
+    while True:
+        if reg_pattern.match(user_data):  # получаем булевое значение|we get a Boolean value
+            if len(users_list) > 0 and data_to_check:
+                if not check_unique_data(user_data, data_to_check):
+                    user_data = input()
+                    continue
+        else:
+            print("Ввод некорректен\nПовторите ввод")
+            user_data = input()
+            continue
+        print(f"{user_data} - данные приняты")
 
 
