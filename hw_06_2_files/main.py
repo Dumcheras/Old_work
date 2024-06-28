d874fa9fd43f736a05e01d0a1308cae0bd023242
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-06-28 22:42:14 +0300

    написан код, выводящий в консоль содержимое папки hw_06_2_files\\the code has been written that outputs the contents of the hw_06_2_files folder to the console

--- hw_06_2_files/main.py
+++ hw_06_2_files/main.py
@@ -4,4 +4,10 @@ import os
 #                                        r'\data_path_1\test_file_1.txt')
 # print(path_to_test_file_1)
 
+into_hw_06 = os.path.abspath(r'C:\Users\Константин\Desktop\Old_work_1\hw_06_2_files')
+print(into_hw_06)
 
+for path, dirnames, filenames in os.walk(into_hw_06):
+    print(f"path - {path}")
+    print(f"dirnames - {dirnames}")
+    print(f"filenames - {filenames}")
