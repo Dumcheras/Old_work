dddbca57854f9f91982732cd70af6defd4890307
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-06-28 22:40:33 +0300

    написан нормализованный путь к файлу test_file_1.txt\\a normalized file path has been written test_file_1.txt

---
+++ hw_06_2_files/main.py
@@ -0,0 +1,7 @@
+import os
+
+# path_to_test_file_1 = os.path.normpath(r'C:\Users\Константин\Desktop\Old_work_1\hw_06_2_files'
+#                                        r'\data_path_1\test_file_1.txt')
+# print(path_to_test_file_1)
+
+
