4fe4de9b4971e4dc124c186df1730a145634b6ce
Konstantin Nuiskov <rannas.konst@gmail.com>
2024-06-29 19:50:35 +0300

    добавлен ввод имени пользователя, выбор уровня сложности через функцию get_user_level\\added user name input, difficulty level selection via the get_user_level function

--- hw_06_json/main_json.py
+++ hw_06_json/main_json.py
@@ -1,7 +1,10 @@
-from hw_06_json.utils.utils import get_data
+from hw_06_json.utils.utils import get_data, get_user_level
 
 questions, levels = get_data(r'questions/questions.json')
 questions_list = questions['questions']
 user_level = levels['levels']
 # print(questions_list)  # список словарей
 # print(user_level)  # словарь
+user_name = input('Введите имя студента:\n')
+user_choice = input('Введите уровень сложности:\nлегкий|средний|сложный\n')
+user_words = get_user_level(user_choice, questions_list)
